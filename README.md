f7f11c8c778cf2558568f5fddbc8726584fea2ba
Eduardo Douglas de Melo Caetano <ecaetano@marlin.net>
2023-01-18 16:24:50 -0300

    exclusao de arquivos errados

---
+++ README.md
@@ -0,0 +1,1 @@
+# Nlw-Setup---Trilha-Ignite
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import fastify from "fastify";
+import cors from "@fastify/cors"
+import { PrismaClient } from "@prisma/client"
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.register(cors, {
+    // origin: ['http://localhost:3000']
+})
+
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany()
+
+    return habits
+})
+
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log('HTTP server running!')
+})
+
