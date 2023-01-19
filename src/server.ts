7a8ac4679d179b12d8d821191a0431ad6a344f13
Eduardo Douglas de Melo Caetano <ecaetano@marlin.net>
2023-01-19 16:06:41 -0300

    estruturacao do banco e rotas backend

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,14 @@
 import fastify from "fastify";
 import cors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client"
+import { appRoutes } from "./lib/routes";
 
 const app = fastify()
-const prisma = new PrismaClient()
 
 app.register(cors, {
     // origin: ['http://localhost:3000']
 })
 
-app.get('/hello', async () => {
-    const habits = await prisma.habit.findMany()
-
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333
